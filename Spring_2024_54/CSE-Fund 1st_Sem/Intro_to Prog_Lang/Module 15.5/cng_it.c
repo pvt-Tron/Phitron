57a71c0500b756ea5ec1d7d6f8c919011c1251c7
pvt-Tron <pv.tassi@gmail.com>
2024-05-02 14:05:06 +0200

    15.5 3

--- Spring_2024_54/CSE-Fund 1st_Sem/Intro_to Prog_Lang/Module 15.5/cng_it.c
+++ Spring_2024_54/CSE-Fund 1st_Sem/Intro_to Prog_Lang/Module 15.5/cng_it.c
@@ -6,15 +6,14 @@ void cng_it(int *arr3, int x){
 int main()
 {
     int n2;
-    scanf("%d ", &n2);
+    scanf("%d", &n2);
     int arr4[n2];
     for(int i = 0; i < n2; i++){
         scanf("%d ", &arr4[i]);
-        printf("%d ", arr4[n2]);
     }
     cng_it(arr4,n2);
     for(int i = 0; i < n2; i++){
-        printf("%d", arr4[n2]);
+        printf("%d", arr4[i]);
         if(i < n2 - 1){printf(" ");}
     }
 
